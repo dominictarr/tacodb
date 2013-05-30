458aae67d78e5ac5313ff656cf78811934286c65
Dominic Tarr <dominic.tarr@gmail.com>
2013-05-30 22:11:57 +0200

    examples

--- examples/db.js
+++ examples/db.js
@@ -1,12 +1,22 @@
-var sublevel = require('level-sublevel')
-var MapReduce = require('map-reduce')
+var sublevel   = require('level-sublevel')
+var MapReduce  = require('map-reduce')
 var multilevel = require('multilevel')
+var static     = require('level-static')
 
 return module.exports = function (db) {
 
   db.options.valueEncoding = 'json'
 
   sublevel(db)
+  console.log('STARTED')
+
+  db.on('connection', function (stream) {
+    console.error('CONNECTION', stream)
+  })
+
+  console.error('http', static(db.sublevel('static')))
+
+  db.on('http_connection', static(db.sublevel('static')))
 
   MapReduce(db, 'map', function (key, value, emit) {
     emit(typeof value, 1) 
