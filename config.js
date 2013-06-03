687270196667bde03d38105a78edfee6deab1481
Dominic Tarr <dominic.tarr@gmail.com>
2013-06-03 22:51:17 +0200

    include default for master address

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ var mkdirp = require('mkdirp')
 var config = module.exports = require('rc')('taco', {
   path: '/tmp/taco-master',
   root: '/tmp/tacos',
-  port: 8000
+  port: 8000,
+  master: 'http://localhost:8000'
 })
 
 mkdirp.sync(config.root)
